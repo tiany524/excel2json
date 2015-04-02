e37f56634e8d776ca728f081a01988e88ce8bf5b
floyda <floyda@163.com>
2015-04-02 12:36:40 +0800

    encoding and config.json

--- README.md
+++ README.md
@@ -3,17 +3,30 @@
 
 ## Overview
 
-通过Python的xlrd库, 导出excel的内容为Json文件.
+通过Python的xlrd库, 导出excel的数据到Json文件.
 
 ## Usage
 
-+ 直接通过Python的环境执行, 需要安装xlrd  
++ 直接通过Python的运行环境执行, 需要安装xlrd  
 
-`pip install xlrd`
+```
+pip install xlrd
+python excel2json
+```
 
-+ 通过exe执行(已经通过py2exe打包好了)  
++ 通过py2exe打包成exe文件.
 
 ```
 cd dist
 ./excel2json.exe
+```
+
+## Setting
+
+config.json:
+```
+{
+	"work_dir"   : ".",
+	"output_dir" : "./output"
+}
 ```
--- excel2json.py
+++ excel2json.py
@@ -4,16 +4,35 @@ from excel import Excel
 import os, json
 
 SUFFIX = "xls"
+DEFAULT_PATH = "./"
 
-# Todo: 通过配置读取excel和导出.
-# class Setting():
-# 	def __init__(self, path):
-# 		fp = open(path, "r")
-# 		print fp
-# 		# self.data = json.read(fp.read())
-# 		fp.close()
-# 
-# setting = Setting("config.json")
+class Setting():
+
+	def __init__(self, path):
+		self.data = self.load(path)
+		self.create_folder(self.work_dir())
+		self.create_folder(self.output_dir())
+
+	def load(self, path):
+		fp = open(path, "r")
+		content = fp.read()
+		fp.close()
+		return json.loads(content)
+
+	def create_folder(self, path):
+		if os.path.exists(path):
+			return
+		else:
+			os.mkdir(path)
+
+	def work_dir(self):
+		return self.data["work_dir"] + "/" or DEFAULT_PATH
+
+	def output_dir(self):
+		return self.data["output_dir"] + "/" or DEFAULT_PATH
+
+
+setting = Setting("config.json")
 
 
 def get_xls_files(path = "."):
@@ -24,32 +43,34 @@ def get_xls_files(path = "."):
 
 	return files
 
+
 def write_json(file_name, data):
-	encodedjson = json.dumps(data, sort_keys=True)
-	# open json file
+	file_name = setting.output_dir() + file_name
+
 	fp = open(file_name, "w")
+	encodedjson = json.dumps(data, sort_keys=True)
 	fp.write(encodedjson)
 	fp.close()
 
-def get_json_keys(excel):
-	return excel.get_row_values(0)
 
 def unic2str(s):
 	if type(s) == unicode:
 		s = s.encode("utf-8")
-	s = str(s) 
-	return s
+	return str(s)
+
 
 def get_data4json(excel):
-	keys = get_json_keys(excel)
+	keys = excel.get_row_values(0)
+	Ids  = excel.get_col_values(0)
+
 	dic = {}
 	for uid in xrange(1 , excel.get_rows()):
-		primary_key = str(uid)
+		primary_key = str(Ids[uid])
 
 		dic[primary_key] = {}
 		row_value = excel.get_row_values(uid)
 		for i in xrange(1, excel.get_cols()):
-			key = unic2str(keys[i])
+			key   = unic2str(keys[i])
 			value = unic2str(row_value[i])
 			
 			dic[primary_key][key] = value
@@ -58,13 +79,15 @@ def get_data4json(excel):
 
 
 def main():
-	for path in get_xls_files():
-		excel = Excel(path)
+	for path in get_xls_files(setting.work_dir()):
+		excel = Excel(setting.work_dir() + path)
 		data = get_data4json(excel)
 		
 		split_pre = path.find(SUFFIX) - 1
 		name = path[:split_pre] + ".json"
-		write_json(name, repr(data))
+
+		write_json(name, data)
+
 
 if __name__ == '__main__':
 	main()
